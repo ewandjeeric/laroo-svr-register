57c4e1c0f6cf178fafc177b0673d35ed8cbc8715
ewandjeeric <ewandjeeric@gmail.com>
2024-07-30 12:49:00 +0100

    .

---
+++ src/test/java/net/laroo/LarooSvrRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package net.laroo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LarooSvrRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
